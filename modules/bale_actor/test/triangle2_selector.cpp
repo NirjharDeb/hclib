e18bccd988e02cb9744956e0af0abab01134e5c2
Nirjhar Deb <ndeb6@gatech.edu>
2024-10-14 19:02:57 +0000

    Fixed triangle counting algo

--- modules/bale_actor/test/triangle2_selector.cpp
+++ modules/bale_actor/test/triangle2_selector.cpp
@@ -1,3 +1,17 @@
+/******************************************************************
+//
+//
+//  Modified code based on original by Institute for Defense Analyses
+//  under BSD-like license.
+//
+//  This software is provided under the terms of the license at the top
+//  of the original code.
+//
+ *****************************************************************/
+/*! \file triangle_counting.cpp
+ * \brief Triangle counting in a lower triangular graph.
+ */
+
 #include <math.h>
 #include <shmem.h>
 extern "C" {
@@ -6,172 +20,210 @@ extern "C" {
 #include <std_options.h>
 #include "selector.h"
 
-#define THREADS shmem_n_pes()
-#define MYTHREAD shmem_my_pe()
+#define NUM_THREADS shmem_n_pes()
+#define MY_PE shmem_my_pe()
 
-typedef struct TrianglePkt {
-    int64_t w;
-    int64_t vj;
-} TrianglePkt;
+typedef struct TriangleMessage {
+    int64_t neighbor_vertex;
+    int64_t current_vertex;
+} TriangleMessage;
 
-enum MailBoxType {REQUEST};
+enum MailBox { REQUEST };
 
-class TriangleSelector: public hclib::Selector<1, TrianglePkt> {
+class TriangleCounter : public hclib::Selector<1, TriangleMessage> {
 public:
-    TriangleSelector(int64_t* cnt, sparsemat_t* mat) : cnt_(cnt), mat_(mat) {
-        mb[REQUEST].process = [this] (TrianglePkt pkt, int sender_rank) { 
-            this->req_process(pkt, sender_rank);
+    TriangleCounter(int64_t* triangle_count, sparsemat_t* lower_matrix)
+        : triangle_count_(triangle_count), lower_matrix_(lower_matrix) {
+        mb[REQUEST].process = [this](TriangleMessage msg, int sender_rank) {
+            this->process_request(msg, sender_rank);
         };
     }
 
 private:
-    int64_t* cnt_;
-    sparsemat_t* mat_;
+    int64_t* triangle_count_;
+    sparsemat_t* lower_matrix_;
 
-    // Process each triangle check for lower triangular matrix
-    void req_process(TrianglePkt pkg, int sender_rank) {
-        int64_t tempCount = 0;
+    void process_request(TriangleMessage msg, int sender_rank) {
+        int64_t triangle_found = 0;
 
-        // Loop over all neighbors of `vj` in the lower triangular matrix
-        for (int64_t k = mat_->loffset[pkg.vj]; k < mat_->loffset[pkg.vj + 1]; k++) {
-            if (pkg.w == mat_->lnonzero[k]) {
-                tempCount++;
+        for (int64_t idx = lower_matrix_->loffset[msg.current_vertex];
+             idx < lower_matrix_->loffset[msg.current_vertex + 1]; idx++) {
+            if (msg.neighbor_vertex == lower_matrix_->lnonzero[idx]) {
+                triangle_found++;
                 break;
             }
-            if (pkg.w < mat_->lnonzero[k]) {
-                break;  // Since the neighbors are sorted, stop early
+            if (msg.neighbor_vertex < lower_matrix_->lnonzero[idx]) {
+                break;
             }
         }
 
-        // Update the global triangle count
-        *cnt_ += tempCount;
+        // Atomically update the triangle count
+        *triangle_count_ += triangle_found;
     }
 };
 
-// Main triangle counting function for the lower triangular matrix
-double triangle_selector(int64_t* count, sparsemat_t* L) {
-    int64_t numpushed = 0;
+double count_triangles(int64_t* triangle_count, int64_t* messages_sent, sparsemat_t* L) {
+    int64_t local_messages = 0;
 
     if (!L) {
-        T0_printf("ERROR: triangle_selector: NULL L!\n");
-        return -1.0;
+        T0_printf("ERROR: count_triangles: NULL matrix L!\n");
+        assert(false);
     }
 
     // Start timing
-    double t1 = wall_seconds();
-  
-    // Selector object for message passing
-    TriangleSelector* triSelector = new TriangleSelector(count, L);
+    double start_time = wall_seconds();
 
-    hclib::finish([=, &numpushed]() {
-        triSelector->start();
-        int64_t k, kk, pe;
-        int64_t l_i, L_i, L_j;
+    TriangleCounter* counter = new TriangleCounter(triangle_count, L);
 
-        TrianglePkt pkg;
+    hclib::finish([=, &local_messages]() {
+        counter->start();
+        int64_t k, kk, dest_pe;
+        int64_t local_row, global_row, neighbor_col;
 
-        // Loop over nonzero elements in the lower triangular matrix `L`
-        for (l_i = 0; l_i < L->lnumrows; l_i++) {
-            // For each row i, loop over all its neighbors j (L_i -> L_j)
-            for (k = L->loffset[l_i]; k < L->loffset[l_i + 1]; k++) {
-                L_i = l_i * THREADS + MYTHREAD;
-                L_j = L->lnonzero[k];
+        TriangleMessage msg;
 
-                // Determine which PE should handle this message
-                pe = L_j % THREADS;
-                pkg.vj = L_j / THREADS;  // Send vertex j
+        // Iterate over each nonzero element in the lower triangular matrix L
+        for (local_row = 0; local_row < L->lnumrows; local_row++) {
+            for (k = L->loffset[local_row]; k < L->loffset[local_row + 1]; k++) {
+                global_row = local_row * NUM_THREADS + MY_PE;
+                neighbor_col = L->lnonzero[k];
 
-                // Loop over the neighbors of i and check for triangles
-                for (kk = L->loffset[l_i]; kk < L->loffset[l_i + 1]; kk++) {
-                    pkg.w = L->lnonzero[kk];  // Send vertex w
+                dest_pe = neighbor_col % NUM_THREADS;
+                msg.current_vertex = neighbor_col / NUM_THREADS;
+                for (kk = L->loffset[local_row]; kk < L->loffset[local_row + 1]; kk++) {
+                    msg.neighbor_vertex = L->lnonzero[kk];
 
-                    // Only count the triangle if w < j to avoid double-counting
-                    if (pkg.w > L_j) {
-                        continue;  // Skip redundant triangle checks
+                    if (msg.neighbor_vertex > neighbor_col) {
+                        break;
                     }
 
-                    numpushed++;
-                    triSelector->send(REQUEST, pkg, pe);
+                    local_messages++;
+                    counter->send(REQUEST, msg, dest_pe);
                 }
             }
         }
-
-        // Done sending all triangle counting messages
-        triSelector->done(REQUEST);
+        // Indicate completion of message sending
+        counter->done(REQUEST);
     });
 
-    // Wait for all PEs to finish
     lgp_barrier();
+    *messages_sent = local_messages;
 
-    t1 = wall_seconds() - t1;
-    return t1;
-}
+    minavgmaxD_t stat[1];
+    double elapsed_time = wall_seconds() - start_time;
+    lgp_min_avg_max_d(stat, elapsed_time, NUM_THREADS);
 
-// Function to validate triangle counts
-void validate_triangle_counts(sparsemat_t* L, int64_t* triangles) {
-    int64_t total_triangles = 0;
-    int64_t total_triangle_counts = 0;
-
-    // Sum the triangles across all PEs
-    for (int64_t i = 0; i < L->lnumrows; i++) {
-        total_triangle_counts += triangles[i];
-    }
-
-    // Reduce the triangle count across all PEs
-    total_triangle_counts = lgp_reduce_add_l(total_triangle_counts);
-    T0_printf("Total triangle counts: %ld\n", total_triangle_counts);
-
-    // Ensure all reductions are finished across PEs
-    lgp_barrier();
+    return elapsed_time;
 }
 
 int main(int argc, char* argv[]) {
-    const char *deps[] = { "system", "bale_actor" };
+    const char* deps[] = {"system", "bale_actor"};
     hclib::launch(deps, 2, [=] {
+        int64_t buffer_count = 1024;
+        int64_t num_rows_per_thread = 10000;
+        int64_t nonzeros_per_row = 35;
+        int64_t read_from_file = 0;
+        char filename[64];
 
-        int64_t l_numrows = 10000;  
-        double t1;
+        double start_time;
+        int64_t i;
+        double erdos_renyi_prob = 0.0;
 
-        int64_t* triangles = (int64_t*) malloc(l_numrows * sizeof(int64_t));
-        if (!triangles) {
-            T0_printf("ERROR: Memory allocation failed for triangles.\n");
-            return -1;
+        int opt;
+        while ((opt = getopt(argc, argv, "hb:n:f:e:")) != -1) {
+            switch (opt) {
+                case 'h':
+                    // Print help message
+                    break;
+                case 'b':
+                    sscanf(optarg, "%ld", &buffer_count);
+                    break;
+                case 'n':
+                    sscanf(optarg, "%ld", &num_rows_per_thread);
+                    break;
+                case 'f':
+                    read_from_file = 1;
+                    sscanf(optarg, "%s", filename);
+                    break;
+                case 'e':
+                    sscanf(optarg, "%lg", &erdos_renyi_prob);
+                    break;
+                default:
+                    break;
+            }
         }
-        memset(triangles, 0, l_numrows * sizeof(int64_t));
 
-        sparsemat_t *L;
+        int64_t total_num_rows = num_rows_per_thread * NUM_THREADS;
+        if (erdos_renyi_prob == 0.0) {
+            erdos_renyi_prob = (2.0 * (nonzeros_per_row - 1)) / total_num_rows;
+            if (erdos_renyi_prob > 1.0)
+                erdos_renyi_prob = 1.0;
+        } else {
+            nonzeros_per_row = erdos_renyi_prob * total_num_rows;
+        }
 
-        int64_t numrows = l_numrows * THREADS;
-        double erdos_renyi_prob = 0.1;
-        L = erdos_renyi_random_graph(numrows, erdos_renyi_prob, UNDIRECTED, NOLOOPS, 12345);
+        T0_fprintf(stderr, "Running triangle counting on %d threads\n", NUM_THREADS);
+        if (!read_from_file) {
+            T0_fprintf(stderr, "Number of rows per thread (-n): %ld\n", num_rows_per_thread);
+            T0_fprintf(stderr, "Erdos-Renyi probability (-e): %g\n", erdos_renyi_prob);
+        }
+
+        double correct_triangle_count = -1;
+
+        sparsemat_t* L;
+
+        if (read_from_file) {
+            L = read_matrix_mm_to_dist(filename);
+            if (!L)
+                assert(false);
+
+            T0_fprintf(stderr, "Reading file %s...\n", filename);
+            T0_fprintf(stderr, "Matrix L has %ld rows/cols and %ld nonzeros.\n", L->numrows, L->nnz);
+
+            if (!is_lower_triangular(L, 0)) {
+                T0_fprintf(stderr, "ERROR: Input matrix is not lower triangular!\n");
+                assert(false);
+            }
+
+            sort_nonzeros(L);
+
+        } else {
+            L = erdos_renyi_random_graph(total_num_rows, erdos_renyi_prob, UNDIRECTED, NOLOOPS, 12345);
+        }
 
         lgp_barrier();
-        if (!L) {
-            T0_printf("ERROR: Failed to generate matrix L!\n");
-            free(triangles);
-            return -1;
+
+        T0_fprintf(stderr, "Matrix L has %ld rows/cols and %ld nonzeros.\n", L->numrows, L->nnz);
+
+        if (!is_lower_triangular(L, 0)) {
+            T0_fprintf(stderr, "ERROR: Matrix L is not lower triangular!\n");
+            assert(false);
         }
 
-        if (L->numrows <= 0 || L->lnumrows <= 0) {
-            T0_printf("ERROR: Invalid matrix dimensions.\n");
-            free(triangles);
-            return -1;
-        }
+        T0_fprintf(stderr, "Starting triangle counting...\n");
+        int64_t local_triangle_count = 0;
+        int64_t total_triangle_count = 0;
+        int64_t local_messages_sent = 0;
+        int64_t total_messages_sent = 0;
 
-        T0_printf("L has %ld rows/cols and %ld nonzeros.\n", L->numrows, L->nnz);
-
+        // Run the triangle counting algorithm
+        T0_fprintf(stderr, "Executing triangle counting algorithm...\n");
+        double elapsed_time = count_triangles(&local_triangle_count, &local_messages_sent, L);
         lgp_barrier();
 
-        T0_printf("Run triangle counting ...\n");
-        t1 = triangle_selector(triangles, L);
-        T0_printf("Triangle counting completed in %8.3lf seconds.\n", t1);
+        total_triangle_count = lgp_reduce_add_l(local_triangle_count);
+        total_messages_sent = lgp_reduce_add_l(local_messages_sent);
+        T0_fprintf(stderr, "  %8.3lf seconds: %16ld triangles\n", elapsed_time, total_triangle_count);
 
-        // Validate triangle counts
-        validate_triangle_counts(L, triangles);
+        if ((correct_triangle_count >= 0) && (total_triangle_count != (int64_t)correct_triangle_count)) {
+            T0_fprintf(stderr, "ERROR: Incorrect triangle count!\n");
+        }
 
-        free(triangles);
+        if (correct_triangle_count == -1) {
+            correct_triangle_count = total_triangle_count;
+        }
 
-        return 0;
+        lgp_barrier();
     });
 
     return 0;
